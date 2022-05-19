a3863a050660f6faff81f3c909830529ef5e7b0a
EvilMadSquirrel <minichev.s.l@gmail.com>
2022-05-19 13:40:24 +0300

    added models for users and posts

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from blog.users.models import UserSubscriber
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(UserSubscriber, related_name='posts', on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return self.title
--- blog/users/models.py
+++ blog/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
 
-# Create your models here.
+from blog.posts.models import Post
+
+
+class UserSubscriber(AbstractUser):
+    user = models.ForeignKey(User, related_name='subscribed', on_delete=models.PROTECT, null=True)
+    subscribers = models.ForeignKey(User, related_name='subscribers', on_delete=models.PROTECT, null=True)
+    readed_posts = models.ForeignKey(Post, related_name='readed_posts', on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.get_full_name()
